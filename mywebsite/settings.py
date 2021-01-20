5aead4a07e6e3c9c4d95b5b64f5c71956b871893
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2021-01-20 21:18:29 +0530

    change static directory

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -153,6 +153,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_file")]
+
+
 DAJAXICE_MEDIA_PREFIX = 'dajaxice'
 
 CRONJOBS = [
