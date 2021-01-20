be1adc9200660b101dd2677b0f3a2bee9d831443
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2021-01-20 22:00:08 +0530

    update

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -148,10 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/mywebsite/main/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_file")]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "/static/"))
 
 
 DAJAXICE_MEDIA_PREFIX = 'dajaxice'
