b211267c83e598b90644f9bef464b24f373daf50
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2021-01-21 09:23:59 +0530

    update articleweb

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/mywebsite/main/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/nand/articleweb/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
