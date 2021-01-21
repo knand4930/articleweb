c5b0c4bd69ae087b97fd2f90df8cbede067f92d6
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2021-01-21 08:51:00 +0530

    update static url

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'home/nand/articleweb/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = '/mywebsite/main/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
