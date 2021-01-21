f3b8b24cc6929762495abf413e284b2bd73fde1a
Nand Kishore <56184819+knand4930@users.noreply.github.com>
2021-01-21 08:43:03 +0530

    update media file

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -147,9 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'home/nand/articleweb/main/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = '/mywebsite/main/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assest")]
